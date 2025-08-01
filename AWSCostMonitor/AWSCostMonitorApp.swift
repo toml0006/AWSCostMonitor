9df994218a7d95d2f825c9ff20c0349844c9e828
Jackson Tomlinson <jackson.tomlinson@gmail.com>
2025-08-01 16:58:42 -0500

    Initial Commit

---
+++ AWSCostMonitor/AWSCostMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWSCostMonitorApp.swift
+//  AWSCostMonitor
+//
+//  Created by Jackson Tomlinson on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AWSCostMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AWSCostMonitorTests/AWSCostMonitorTests.swift
@@ -0,0 +1,17 @@
+//
+//  AWSCostMonitorTests.swift
+//  AWSCostMonitorTests
+//
+//  Created by Jackson Tomlinson on 8/1/25.
+//
+
+import Testing
+@testable import AWSCostMonitor
+
+struct AWSCostMonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
